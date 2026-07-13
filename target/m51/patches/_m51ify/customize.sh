a5e3454b3ef88f31a912907a25e204bffda351e9
JoJo <102670629+jmengelsk@users.noreply.github.com>
2026-07-13 21:48:11 +0300

    Update customize.sh

--- target/m51/patches/_m51ify/customize.sh
+++ target/m51/patches/_m51ify/customize.sh
@@ -100,7 +100,7 @@ EVAL "sed -i 's|atoll|sm6150|g' \"$WORK_DIR/vendor/etc/vramdiskd.xml\" \"$WORK_D
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Injecting m51-specific blobs"
-EVAL "git clone \"https://github.com/mehedihjoy0/M51-Device-Tree\" \"$MODPATH/tree\""
+EVAL "git clone \"https://jmengelsk/mehedihjoy0/M51-Device-Tree\" \"$MODPATH/tree\""
 ADD_TO_WORK_DIR "$MODPATH/tree" "system" "."
 ADD_TO_WORK_DIR "$MODPATH/tree" "vendor" "."
 EVAL "cp -r \"$WORK_DIR/vendor/etc/media_profiles_V1_0.xml\" \"$WORK_DIR/odm/etc\""
