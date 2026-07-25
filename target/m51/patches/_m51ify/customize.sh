0939c333d2789c80581dea6d782f843e242ef959
Josef Engelskirchen <josef.engelskirchen@gmail.com>
2026-07-25 20:49:39 +0300

    build date

--- target/m51/patches/_m51ify/customize.sh
+++ target/m51/patches/_m51ify/customize.sh
@@ -66,6 +66,8 @@ SET_PROP "vendor" "ro.netflix.bsp_rev" "Q7250-19133-1"
 SET_PROP "vendor" "ro.bootimage.build.fingerprint" "samsung/m51nsxx/m51:11/RP1A.200720.012/M515FXXU6DXJ2:user/release-keys"
 SET_PROP "odm" "ro.odm.build.fingerprint" "samsung/r0qxxx/r0q:12/SP1A.210812.016/S901EXXSEGZE3:user/release-keys"
 SET_PROP "system" "ro.system.build.fingerprint" "samsung/r0qxxx/qssi:16/BP2A.250605.031.A3/S901EXXSEGZE3:user/release-keys"
+SET_PROP "system" "ro.system.build.date" "${date}"
+SET_PROP "system" "ro.system.build.date.utc" "${date +%s}"
 SET_PROP "vendor" "ro.vendor.build.fingerprint" "samsung/m51nsxx/m51:11/RP1A.200720.012/M515FXXU6DXJ2:user/release-keys"
 
 SET_PROP "odm" "ro.product.odm.device" "m51"
