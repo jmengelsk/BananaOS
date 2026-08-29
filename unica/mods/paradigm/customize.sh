74913d4037983f6ee1f6519897b1faef6a82e732
JoJo <102670629+jmengelsk@users.noreply.github.com>
2026-08-30 02:12:23 +0300

    Update customize.sh

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -160,16 +160,16 @@ ADD_TO_WORK_DIR "pa2qxxx" "system" \
     "system/priv-app/MediaSearch/MediaSearch.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa2qxxx" "system" \
     "system/priv-app/SemanticSearchCore/SemanticSearchCore.apk" 0 0 644 "u:object_r:system_file:s0"
-DECODE_APK "system" "system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk"
-LOG "- Enabling Semantic search feature in /system/system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk"
+#DECODE_APK "system" "system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk"
+#LOG "- Enabling Semantic search feature in /system/system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk"
 #EVAL "cp -a \"$MODPATH/semanticsearch/SecSettingsIntelligence.apk/res/raw/\"* \"$APKTOOL_DIR/system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk/res/raw\""
 #SMALI_PATCH "system" "system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk" \
 #    "smali_classes2/com/samsung/android/settings/intelligence/Rune.smali" "replaceall" \
 #    "const-string v1, \\\"\\\"" \
 #    "const-string v1, \\\"400\\\"" \
 #    > /dev/null
-SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MSCH_SUPPORT_NLSEARCH" "TRUE"
-LOG_STEP_OUT
+#SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MSCH_SUPPORT_NLSEARCH" "TRUE"
+#LOG_STEP_OUT
 
 # Game Booster
 LOG "- Downloading latest Game Booster app"
